026e54ee2cfeba05fc778ae77efda0f065753023
HabibIbrahimov <habibyi@code.edu.az>
2022-05-27 12:04:19 +0400

    feat:add change event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password';
-  password='';
+ includeLetters:boolean=false;
+ includeNumbers:boolean=false;
+ includeSymbols:boolean=false;
+
+  modifyLetters(){
+     this.includeLetters=!this.includeLetters;
+  }
+
+  modifyNumbers(){
+    this.includeNumbers=!this.includeNumbers;
+  }
+
+  modifySymbols(){
+    this.includeSymbols=!this.includeSymbols;
+  }
 
   buttonClick(){
-   
+   console.log(`Degerlerim;
+   Letters:${this.includeLetters}
+   Numbers:${this.includeNumbers}
+   Symbols:${this.includeSymbols}
+   `);
   }
 
   getName()
   {
-    
+
   }
 }
