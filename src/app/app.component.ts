44bd281da37578d777450173990ba8214e0fbedb
HabibIbrahimov <habibyi@code.edu.az>
2022-05-27 12:52:03 +0400

    feat:Creating a Password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  length:number=0;
+ password='';
+ length:number=0;
  includeLetters:boolean=false;
  includeNumbers:boolean=false;
  includeSymbols:boolean=false;
@@ -34,15 +35,35 @@ export class AppComponent {
   }
 
   buttonClick(){
-   console.log(`Degerlerim;
-   Letters:${this.includeLetters}
-   Numbers:${this.includeNumbers}
-   Symbols:${this.includeSymbols}
-   `);
+   const numbers='1234567890';
+   const letters='abcdefghijklmnopqrstuvwyz';
+   const symbols='!@#$%^&*()';
+   
+   let validChars='';
+
+   if(this.includeLetters)
+   {
+     validChars += letters;
+   }
+   if(this.includeNumbers)
+   {
+     validChars += numbers;
+   }
+   if(this.includeSymbols)
+   {
+     validChars += symbols;
+   }
+
+   let generatedPassword = '';
+   for(let i=0; i<this.length;i++)
+   {
+     const index=Math.floor(Math.random()*validChars.length);
+     generatedPassword +=validChars[index];
+   }
+
+   this.password=generatedPassword;
+
   }
 
-  getName()
-  {
-
-  }
+  
 }
